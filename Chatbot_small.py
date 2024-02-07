27d50e2e3110351ba83e3b7bafe9b07f59fa21ba
Tejveer Singh <65494424+Tej752@users.noreply.github.com>
2024-02-07 12:57:10 +0530

    Conversational Chatbot using Gemini Pro

---
+++ Chatbot_small.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+
+
+genai.configure(api_key='AIzaSyDvmT5BLMRcaJxbC06GN-uHVYadO0SfU7E')
+model=genai.GenerativeModel('gemini-pro')
+
+chat=model.start_chat(history=[])
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+
+st.set_page_config(page_title='Q/A Session')
+st.header("Conversation Model")
+
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
+    
+input=st.text_input("Input:",key="input")
+submit=st.button("Ask Question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader('The response:')
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot",chunk.text))
+st.subheader('Chat History: ')
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
+
+
